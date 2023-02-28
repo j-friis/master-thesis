668ec4615427d5b4cb0cf8ca6ec2ca56dd8687af
j-friis <xdr476@alumni.ku.dk>
2023-02-28 23:30:11 +0100

    Mulit process atempt

--- Code/scripts/pdal/cal_height.py
+++ Code/scripts/pdal/cal_height.py
@@ -4,6 +4,8 @@ import os
 from os import listdir
 from os.path import isfile, join
 from tqdm import tqdm
+from multiprocessing import Pool
+from tqdm.contrib.concurrent import process_map
 
 # out_file = file_name.split(".")[0]
 
@@ -22,32 +24,62 @@ from tqdm import tqdm
 # ]
 # """ % (file_name, out_file)
 
+def pipeline(in_file: str):
+    print(in_file)
+    file_name = in_file
+    file_name = join(dir, file_name)
+    out_file = file_name.split(".")[0]
+    out_file = join(dir, out_file)
+    json = """
+    [
+        "%s",
+        {
+            "type":"filters.hag_nn"
+        },
+        {
+            "type":"writers.las",
+            "filename":"%s_hag_nn.laz",
+            "extra_dims":"HeightAboveGround=float32",
+            "compression":"laszip"
+        }
+    ]
+    """ % (file_name, out_file)
+    pipeline = pdal.Pipeline(json)
+    count = pipeline.execute()
+
+
 def cal_height(dir: str):
     onlyfiles = [f for f in listdir(dir) if isfile(join(dir, f)) 
-                    and "_hag_delaunay" not in f and ".tif" not in f and "height" not in f]
+                    and "_hag" not in f and ".tif" not in f and "height" not in f]
     print(onlyfiles)
 
-    for file in tqdm(onlyfiles):
-        file_name = file
-        file_name = join(dir, file_name)
-        out_file = file_name.split(".")[0]
-        out_file = join(dir, out_file)
-        json = """
-        [
-            "%s",
-            {
-                "type":"filters.hag_nn"
-            },
-            {
-                "type":"writers.las",
-                "filename":"%s_hag_nn.laz",
-                "extra_dims":"HeightAboveGround=float32",
-                "compression":"laszip"
-            }
-        ]
-        """ % (file_name, out_file)
-        pipeline = pdal.Pipeline(json)
-        count = pipeline.execute()
+    process_map(pipeline, onlyfiles, max_workers=5)
+
+    # with Pool(3) as p:
+    #     p.map(pipeline, onlyfiles)
+
+
+    # for file in tqdm(onlyfiles):
+    #     file_name = file
+    #     file_name = join(dir, file_name)
+    #     out_file = file_name.split(".")[0]
+    #     out_file = join(dir, out_file)
+    #     json = """
+    #     [
+    #         "%s",
+    #         {
+    #             "type":"filters.hag_nn"
+    #         },
+    #         {
+    #             "type":"writers.las",
+    #             "filename":"%s_hag_nn.laz",
+    #             "extra_dims":"HeightAboveGround=float32",
+    #             "compression":"laszip"
+    #         }
+    #     ]
+    #     """ % (file_name, out_file)
+    #     pipeline = pdal.Pipeline(json)
+    #     count = pipeline.execute()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Calculate the height from the ground.')
