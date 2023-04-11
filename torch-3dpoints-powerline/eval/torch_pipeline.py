48275243aad1b4923e0746802cad2a62f9a51198
j-friis <xdr476@alumni.ku.dk>
2023-04-11 14:13:18 +0200

    Use correct configs

--- torch-3dpoints-powerline/eval/torch_pipeline.py
+++ torch-3dpoints-powerline/eval/torch_pipeline.py
@@ -12,17 +12,21 @@ if __name__ == "__main__":
     parser.add_argument('folder', type=str, help='Folder with laz files to predict on')
     parser.add_argument('model', type=str, help='Path to the model')
 
-    args = parser.parse_args()
-    data_path = args.folder
-    model_path = args.model
+    # args = parser.parse_args()
+    # data_path = args.folder
+    # model_path = args.model
+
+    data_path = "/home/jf/msc_data"
+    model_path = "/home/jf/Documents/msc/torch-3dpoints-powerline/outputs/2023-04-09/13-31-16/SEUnet1850Metersblock50cmvoxel.pt"
 
     model, transform_test, config = load_model(model_path, data_path)
 
     ## load transform pt pre
-    processed_folder_name = config['run_config']['data']["processed_folder"] 
-    data_root_path = config['run_config']['data']['dataroot'] + "/denmark"
+    processed_folder_name = config["processed_folder"] 
+    data_root_path = os.path.join(config['dataroot'] , "denmark")
     processed_data_root_path = os.path.join(data_root_path, processed_folder_name)
-    test_folder = os.path.join(processed_data_root_path, "test_0_(0.1, 0.1)")
+    test_folder_name = f"test_0_({config['block_size_x']}, {config['block_size_y']})"
+    test_folder = os.path.join(processed_data_root_path, test_folder_name)
     pre_trans_path = os.path.join(test_folder, "stats.pt")
     vis_folder =  os.path.join(processed_data_root_path, 'vis')
 
