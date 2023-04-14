bf05ac47b5be84c97b74d86b54f850da09f8eca0
j-friis <xdr476@alumni.ku.dk>
2023-04-14 15:34:34 +0200

    Add scripts for starting train easier

---
+++ torch-3dpoints-powerline/start_training_no_pdal_20cm_ones.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#BATCH --job-name=minkowskiTrain
+#SBATCH --ntasks=1 --cpus-per-task=12 --mem=64000M
+#SBATCH -p gpu --gres=gpu:titanrtx:1
+#SBATCH --time=4-00:00:00
+
+echo "Training Started"
+module load cuda/11.3
+
+eval "$(conda shell.bash hook)"
+conda activate powerlines
+bash seg_sh/sb_train_denmank_pl_hough_20cm_ones.sh
+
+
+
