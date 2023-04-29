97d30124804a363d66e6182b9bbeb6b2c36a2c84
j-friis <xdr476@alumni.ku.dk>
2023-04-29 13:09:14 +0200

    Add command to see gpu

--- torch-3dpoints-powerline/start_training_no_pdal_20cm.sh
+++ torch-3dpoints-powerline/start_training_no_pdal_20cm.sh
@@ -11,6 +11,11 @@ eval "$(conda shell.bash hook)"
 conda activate powerlines
 bash seg_sh/sb_train_denmank_pl_hough_20cm.sh
 python -c "import MinkowskiEngine as ME; ME.print_diagnostics()"
-
-
-
+python -c "import torch; print('Cuda is available: ', torch.cuda.is_available())"
+python -c "import torch; print('Torch version: ',torch.__version__)"
+python -c "import torch; print('Torch version cuda: ', torch.version.cuda)"
+python -c "import torch; torch.cuda.device_count()"
+python -c "import torch; torch.cuda.current_device()"
+python -c "import torch; torch.cuda.device(0)"
+python -c "import torch; torch.cuda.get_device_name(0)"
+nvidia-smi -L
