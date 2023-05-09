bb1457d180022b1cba0c70c9e76965576ac71b92
j-friis <xdr476@alumni.ku.dk>
2023-05-09 14:31:40 +0200

    Change learning rate and remove batch normilze

--- torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_hough_20cm_ones.sh
+++ torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_hough_20cm_ones.sh
@@ -1,5 +1,6 @@
 
 CUDA_VISIABLE_DEVICES=1 HYDRA_FULL_ERROR=1 python -u train.py task=segmentation models=segmentation/minkowski  \
-model_name=ResUNet50 data=segmentation/den_pl_hough_small_20cm_ones training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified \
-training.wandb.name=minkowski_powerline_segmentation_ResUNet50_20cm_ones training.batch_size=32
+model_name=ResUNet50 data=segmentation/den_pl_hough_small_20cm_ones \
+training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified training.wandb.name=minkowski_powerline_segmentation_ResUNet50_20cm_ones \
+training=denmark/minkowski training.batch_size=32 lr_scheduler=exponential lr_scheduler.params.gamma=0.998
 
