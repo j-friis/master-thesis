7e3e6831b13eab735569cf6bf4c288c37ed227ca
j-friis <xdr476@alumni.ku.dk>
2023-05-09 09:25:45 +0200

    Try ResNet

--- torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_hough_20cm_ones.sh
+++ torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_hough_20cm_ones.sh
@@ -1,5 +1,5 @@
 
 CUDA_VISIABLE_DEVICES=1 HYDRA_FULL_ERROR=1 python -u train.py task=segmentation models=segmentation/minkowski  \
-model_name=SEUNet50 data=segmentation/den_pl_hough_small_20cm_ones training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified \
-training.wandb.name=minkowski_powerline_segmentation_SEUNet50_20cm_ones training.batch_size=32
+model_name=ResUNet50_ data=segmentation/den_pl_hough_small_20cm_ones training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified \
+training.wandb.name=minkowski_powerline_segmentation_ResUNet50_20cm_ones training.batch_size=32
 
