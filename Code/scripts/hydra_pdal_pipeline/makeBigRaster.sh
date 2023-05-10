62e0cd39d390c412b5df8d8dd0fe45ad27b11e59
j-friis <xdr476@alumni.ku.dk>
2023-05-10 16:03:48 +0200

    Remove wrong arguments

--- Code/scripts/hydra_pdal_pipeline/makeBigRaster.sh
+++ Code/scripts/hydra_pdal_pipeline/makeBigRaster.sh
@@ -3,6 +3,6 @@
 #SBATCH --ntasks=1 --cpus-per-task=12 --mem=64000M
 #SBATCH --time=1-00:00:00
 
-python raster.py ~/data/denmark/raw/train 4 3
-python raster.py ~/data/denmark/raw/test 4 3
-python raster.py ~/data/denmark/raw/val 4 3
+python raster.py ~/data/denmark/raw/train
+python raster.py ~/data/denmark/raw/test
+python raster.py ~/data/denmark/raw/val
