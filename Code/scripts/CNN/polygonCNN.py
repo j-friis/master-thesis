4eea742f2c1c58142d774d3177e38c994f0a1479
Frederik Noe <frederik.a.noe@hotmail.com>
2023-05-10 10:31:10 +0200

    Update polygonCNN.py

--- Code/scripts/CNN/polygonCNN.py
+++ Code/scripts/CNN/polygonCNN.py
@@ -20,6 +20,83 @@ from shapely.geometry import Polygon, mapping
 from PIL import Image
 from matplotlib.path import Path as plt_path
 
+class conv_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+    
+    def forward(self, inputs):
+        x = self.conv1(inputs)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        return x
+    
+class encoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.conv = conv_block(in_c, out_c)
+        self.pool = nn.MaxPool2d((2, 2))
+
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        p = self.pool(x)
+        return x, p
+    
+class decoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
+        self.conv = conv_block(out_c+out_c, out_c)
+    
+    def forward(self, inputs, skip):
+        x = self.up(inputs)
+        x = torch.cat([x, skip], axis=1)
+        x = self.conv(x)
+        return x
+    
+class ConvUNet(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        #""" Encoder """
+        self.e1 = encoder_block(1, 64).cuda()
+        self.e2 = encoder_block(64, 128).cuda()
+        self.e3 = encoder_block(128, 256).cuda()
+        self.e4 = encoder_block(256, 512).cuda()
+        #""" Bottleneck """
+        self.b = conv_block(512, 1024).cuda()
+        #""" Decoder """
+        self.d1 = decoder_block(1024, 512).cuda()
+        self.d2 = decoder_block(512, 256).cuda()
+        self.d3 = decoder_block(256, 128).cuda()
+        self.d4 = decoder_block(128, 64).cuda()
+        #""" Classifier """
+        self.outputs = nn.Conv2d(64, 1, kernel_size=1, padding=0).cuda()
+    
+    def forward(self, inputs):
+        #""" Encoder """
+        s1, p1 = self.e1(inputs)
+        s2, p2 = self.e2(p1)
+        s3, p3 = self.e3(p2)
+        s4, p4 = self.e4(p3)
+
+        #""" Bottleneck """
+        b = self.b(p4)
+
+        #""" Decoder """
+        d1 = self.d1(b, s4)
+        d2 = self.d2(d1, s3)
+        d3 = self.d3(d2, s2)
+        d4 = self.d4(d3, s1)
+
+        #""" Classifier """
+        outputs = self.outputs(d4)
+        return outputs
+
+
 class PolygonCNN(object):
     def __init__(self, path_to_data, path_to_model, network_size, image_size):
         self.path_to_data = path_to_data
