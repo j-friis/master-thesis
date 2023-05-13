8edc9864b0d52e9e08249d562ebf627cf2de96ac
j-friis <xdr476@alumni.ku.dk>
2023-05-13 09:30:33 +0200

    Try deeper model

--- torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_cnn_20cm_ones.sh
+++ torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_cnn_20cm_ones.sh
@@ -1,6 +1,6 @@
 
 CUDA_VISIABLE_DEVICES=1 HYDRA_FULL_ERROR=1 python -u train.py task=segmentation models=segmentation/minkowski  \
-model_name=SEUNet18 data=segmentation/den_pl_cnn_20cm_ones \
-training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified training.wandb.name=CNN_SEUNet18_20cm_ones \
+model_name=SEUNet50 data=segmentation/den_pl_cnn_20cm_ones \
+training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified training.wandb.name=CNN_SEUNet50_20cm_ones \
 training=denmark/minkowski training.batch_size=32 lr_scheduler=exponential lr_scheduler.params.gamma=0.998
 
