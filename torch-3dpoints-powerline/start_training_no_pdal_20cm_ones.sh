49f519ab4a30959c47ff945acb0899b5df0d8f10
j-friis <xdr476@alumni.ku.dk>
2023-05-15 17:47:36 +0200

    Try different config

--- torch-3dpoints-powerline/start_training_no_pdal_20cm_ones.sh
+++ torch-3dpoints-powerline/start_training_no_pdal_20cm_ones.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #BATCH --job-name=OnesRes
-#SBATCH --ntasks=1 --cpus-per-task=12 --mem=64000M
+#SBATCH --ntasks=1 --cpus-per-task=8 --mem=64000M
 #SBATCH -p gpu --gres=gpu:a100:1
-#SBATCH --time=1-12:00:00
+#SBATCH --time=0-16:00:00
 
 echo "Training Started"
 module load cuda/11.3
