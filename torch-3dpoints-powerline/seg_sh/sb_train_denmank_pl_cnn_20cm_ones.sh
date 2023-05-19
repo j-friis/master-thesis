cc31906d3b55bf455af08129490985390ef09ed1
j-friis <xdr476@alumni.ku.dk>
2023-05-19 23:23:46 +0200

    a

--- torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_cnn_20cm_ones.sh
+++ torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_cnn_20cm_ones.sh
@@ -1,5 +1,5 @@
 
 CUDA_VISIABLE_DEVICES=1 HYDRA_FULL_ERROR=1 python -u train.py task=segmentation models=segmentation/minkowski  \
-model_name=SEUNet50 data=segmentation/den_pl_cnn_20cm_ones \
-training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified training.wandb.name=CNN_SEUNet50_20cm_ones_500 \
+model_name=ResUNet34 data=segmentation/den_pl_cnn_20cm_ones \
+training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified training.wandb.name=CNN_ResUNet34_20cm_ones_500 \
 training=denmark/minkowski training.batch_size=32 lr_scheduler=exponential lr_scheduler.params.gamma=0.998
