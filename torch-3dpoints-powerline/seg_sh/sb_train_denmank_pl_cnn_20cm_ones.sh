fbfc96d8020bcde6304af7e8e3528d0984509796
j-friis <xdr476@alumni.ku.dk>
2023-05-19 10:03:17 +0200

    Try more epochs

--- torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_cnn_20cm_ones.sh
+++ torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_cnn_20cm_ones.sh
@@ -1,6 +1,5 @@
 
 CUDA_VISIABLE_DEVICES=1 HYDRA_FULL_ERROR=1 python -u train.py task=segmentation models=segmentation/minkowski  \
-model_name=ResUNet18 data=segmentation/den_pl_cnn_20cm_ones \
-training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified training.wandb.name=CNN_ResUNet18_20cm_ones \
-training=denmark/minkowski training.batch_size=32 lr_scheduler=exponential lr_scheduler.params.gamma=0.998
-
+model_name=SEUNet18 data=segmentation/den_pl_cnn_20cm_ones \
+training.wandb.log=True training.wandb.project=NewDenmark-Seg-modified training.wandb.name=CNN_SEUNet18_20cm_ones_500 \
+training=denmark/minkowski training.batch_size=32
