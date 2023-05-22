8c067bc2fcf4f9b865bd9846750a69bbfd0b48ce
j-friis <xdr476@alumni.ku.dk>
2023-05-22 16:59:04 +0200

    Increase traning time

--- torch-3dpoints-powerline/start_training_no_pdal_20cm_ones.sh
+++ torch-3dpoints-powerline/start_training_no_pdal_20cm_ones.sh
@@ -2,7 +2,7 @@
 #BATCH --job-name=OnesRes
 #SBATCH --ntasks=1 --cpus-per-task=8 --mem=64000M
 #SBATCH -p gpu --gres=gpu:a100:1
-#SBATCH --time=0-16:00:00
+#SBATCH --time=1-8:00:00
 
 echo "Training Started"
 module load cuda/11.3
