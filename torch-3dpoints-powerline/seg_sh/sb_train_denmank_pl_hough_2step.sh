25680546ed18d298933cae782de3b4b6fab395a0
j-friis <xdr476@alumni.ku.dk>
2023-05-22 16:55:57 +0200

    Use correct height filter

--- torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_hough_2step.sh
+++ torch-3dpoints-powerline/seg_sh/sb_train_denmank_pl_hough_2step.sh
@@ -8,9 +8,9 @@ echo "Runing pdal pipeline"
 eval "$(conda shell.bash hook)"
 
 conda activate pdal
-python torch_points3d/core/data_transform/pdal/run_pipeline.py ~/data/denmark/raw/train $n_cpu 3
-python torch_points3d/core/data_transform/pdal/run_pipeline.py ~/data/denmark/raw/test $n_cpu 3
-python torch_points3d/core/data_transform/pdal/run_pipeline.py ~/data/denmark/raw/val $n_cpu 3
+python torch_points3d/core/data_transform/pdal/run_pipeline.py ~/data/denmark/raw/train $n_cpu 3.5
+python torch_points3d/core/data_transform/pdal/run_pipeline.py ~/data/denmark/raw/test $n_cpu 3.5
+python torch_points3d/core/data_transform/pdal/run_pipeline.py ~/data/denmark/raw/val $n_cpu 3.5
 
 conda activate powerlines
 bash seg_sh/sb_train_denmank_pl_hough.sh
