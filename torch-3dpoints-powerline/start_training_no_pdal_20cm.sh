2e1ee7df6a8e1173d3bb7cd89b832694b339e692
j-friis <xdr476@alumni.ku.dk>
2023-05-26 23:23:45 +0200

    Reduce time

--- torch-3dpoints-powerline/start_training_no_pdal_20cm.sh
+++ torch-3dpoints-powerline/start_training_no_pdal_20cm.sh
@@ -2,7 +2,7 @@
 #BATCH --job-name=Res
 #SBATCH --ntasks=1 --cpus-per-task=12 --mem=64000M
 #SBATCH -p gpu --gres=gpu:a100:1
-#SBATCH --time=2-00:00:00
+#SBATCH --time=1-00:00:00
 
 echo "Training Started"
 module load cuda/11.3
